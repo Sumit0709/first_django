4380183d6879f5a8349a5d23dbf6d8fb8c6d1349
Sumit Ranjan Choudhary <sumitranjan327@gmail.com>
2024-12-20 23:50:20 +0530

    added generic views

--- polls/views.py
+++ polls/views.py
@@ -3,37 +3,65 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 
 from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-    return render(request, "polls/index.html", context)
-
-def home(request):
-    return HttpResponse("Hello, world. You're at the HOME page.")
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-# Questions related views
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-        choices = Choice.objects.all().filter(question_id=question.id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question": question, "choices": choices})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+
+
+                                
+                                ### WITHOUT GENERIC VIEW ###
+
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+
+#     return render(request, "polls/index.html", context)
+
+# def home(request):
+#     return HttpResponse("Hello, world. You're at the HOME page.")
+
+
+# # Questions related views
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#         choices = Choice.objects.all().filter(question_id=question.id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question, "choices": choices})
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
